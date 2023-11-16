3679f93265042206de09ac23b97b3175411b7976
Pham Ngoc Hung <phamngochung@Phams-MacBook-Pro.local>
2023-11-17 02:45:07 +0700

    additional redis

---
+++ food/src/main/java/com/example/securityhibernate/config/TestRedis.java
@@ -0,0 +1,26 @@
+package com.example.securityhibernate.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Component;
+
+import java.util.Set;
+
+@Component
+public class TestRedis implements CommandLineRunner {
+
+    @Autowired
+    private RedisTemplate template;
+
+    @Override
+    public void run(String... args) throws Exception {
+        template.opsForValue().set("pmc", "pmc hcm");
+
+        Set<String> keys = template.keys("*");
+
+        // In ra tất cả các key
+        keys.forEach(System.out::println);
+    }
+
+}
